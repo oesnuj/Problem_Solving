1a43040b51ee2c7aac2e49f5e90a21377091d276
Junseo Kim <112786665+oesnuj@users.noreply.github.com>
2024-01-06 23:54:57 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int input;
+    cin >> input;
+        for(int i = 1; i<10; i++)
+        {
+            cout << input << " * " << i << " = " << i*input << endl;
+        }
+        
+    return 0;
+}
