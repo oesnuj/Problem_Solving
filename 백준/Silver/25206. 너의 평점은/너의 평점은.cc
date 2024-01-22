30985a7fc4b3e06cd1e2ab31f3380aeebe6c9d46
Junseo Kim <semiwest2@gmail.com>
2024-01-23 02:17:09 +0900

    [Silver V] Title: 너의 평점은, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.cc
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.cc
@@ -10,30 +10,21 @@ int main()
 	for (int i = 0; i < 20; i++)
 	{
 		cin >> subject >> credit >> grade;
-		if (grade == "A+")
-			score += (4.5 * credit);
-		else if (grade == "A0")
-			score += (4.0 * credit);
-		else if (grade == "B+")
-			score += (3.5 * credit);
-		else if (grade == "B0")
-			score += (3.0 * credit);
-		else if (grade == "C+")
-			score += (2.5 * credit);
-		else if (grade == "C0")
-			score += (2.0 * credit);
-		else if (grade == "D+")
-			score += (1.5 * credit);
-		else if (grade == "D0")
-			score += (1.0 * credit);
+		if (grade == "F")
+		{
+			score += (0.0 * credit);
+			sum_credit += credit;
+		}
 		else if (grade == "P")
 			continue;
 		else
-			score += (0.0 * credit);
+		{
+			double gradValue = (grade[1] == '+') ? (69 - grade[0] + 0.5) : (69 - grade[0]);
+			score += credit * gradValue;
+			sum_credit += credit;
 
-		sum_credit += credit;
+		}
 	}
-
 	double result = score / sum_credit;
 	cout << result;
 	return 0;
