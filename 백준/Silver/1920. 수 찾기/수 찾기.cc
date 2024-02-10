a14dcc20ceb57ce852f961f047037b936f11e934
Junseo Kim <semiwest2@gmail.com>
2024-02-10 19:35:09 +0900

    [Silver IV] Title: 수 찾기, Time: 172 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -21,7 +21,11 @@ int main()
     sort(input_array, input_array + n); //이진 탐색을 위해 정렬
     
     for (int i = 0; i < m; i++)
-        cout << BinarySearch(input_array, n, search_array[i]) << '\n';
+        cout << BinarySearch(input_array, n, search_array[i]) << '\n'; 
+    //줄바꿈에 endl 사용시 시간초과발생
+    // ***endl 과 \n 차이***
+    //endl은 줄바꿈 후 출력버퍼를 비우는 과정을 수행하기에 많은 라인을 출력할경우 시간이 많이 걸린다.
+    //\n은 출력버퍼는 그대로 놔두고 줄바꿈만 수행한다.
 
     return 0;
 }
