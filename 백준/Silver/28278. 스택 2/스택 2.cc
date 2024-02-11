385f70c6aebc5cd9de2b96e14fdaef14bb9bba77
Junseo Kim <semiwest2@gmail.com>
2024-02-11 18:26:25 +0900

    [Silver IV] Title: 스택 2, Time: 112 ms, Memory: 6300 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	stack <int> stack;
+	int N;
+	cin >> N;
+	char input;
+
+	string str;
+	while (N--)
+	{
+		int x;
+		cin >> input;
+		if (input == '1')
+		{
+			cin >> x;
+			stack.push(x);
+		}
+
+		else if (input == '2')
+		{
+			if (!stack.empty())
+			{
+				cout << stack.top() << '\n';
+				stack.pop();
+			}
+			else
+				cout << -1 << '\n';
+		}
+
+		else if (input == '3')
+			cout << stack.size() << '\n';
+
+		else if (input == '4')
+			cout << stack.empty() << '\n';
+
+		else if (input == '5')
+		{
+			if (!stack.empty())
+				cout << stack.top() << '\n';
+			else
+				cout << -1 << '\n';
+		}
+	}
+	return 0;
+}
