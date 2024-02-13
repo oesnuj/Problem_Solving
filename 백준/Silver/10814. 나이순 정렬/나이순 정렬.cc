6dc34f87533ac8a0f7472bc3bb9e6133a78db226
Junseo Kim <semiwest2@gmail.com>
2024-02-13 20:42:46 +0900

    Update 나이순 정렬.cc

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <stack>
 #include <algorithm>
 using namespace std;
 
@@ -13,7 +12,6 @@ bool compareInfo(const info& a, const info& b) {
 	return a.age < b.age;
 }
 
-
 int main() {
 	int n;
 	cin >> n;
@@ -21,12 +19,13 @@ int main() {
 	for (int i = 0; i < n; i++)
 		cin >> users[i].age >> users[i].name;
 
-	stable_sort(users, users + n, compareInfo);
+	stable_sort(users, users + n, compareInfo); 
+	//1. 구조체와 같이 자료형이 복잡한 경우 정렬시 비교조건을 붙여야한다.
+	//2. stable_sort는 퀵sort와 다르게 동일한 키 값을 가지는 요소들 간의 순서가 유지된다.
 
 	for (int i = 0; i < n; i++)
 		cout << users[i].age << " " << users[i].name << '\n';
 
 	delete[] users;
-
 	return 0;
 }
