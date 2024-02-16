1f922d9f9b0da54739d9963d77c63f95f9c78f43
Junseo Kim <semiwest2@gmail.com>
2024-02-16 23:24:36 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char n; cin >> n;
+    if(n == '0')
+        cout << "YONSEI";
+    else
+        cout << "Leading the Way to the Future";
+	return 0;
+}
