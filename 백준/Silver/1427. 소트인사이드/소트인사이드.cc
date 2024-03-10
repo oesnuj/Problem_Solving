65c8125015b99c6c62b47dacf3f878522e0cc657
Junseo Kim <semiwest2@gmail.com>
2024-03-10 21:13:04 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string n;
+    getline(cin, n);
+    sort(n.begin(), n.end(), greater<char>());
+    cout << n;
+
+    return 0;
+}
