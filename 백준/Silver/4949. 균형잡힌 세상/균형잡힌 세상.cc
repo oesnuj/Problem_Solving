1442aa14933bd480934519eff82c27aeb9765ee6
Junseo Kim <semiwest2@gmail.com>
2024-03-12 01:49:13 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 144 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    while (1)
+    {
+        stack <char> s;
+        string str;
+        getline(cin, str);
+
+        if (str== ".")
+            return 0;
+
+        bool flag = true;
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (str[i] == '(' || str[i] == '[')
+                s.push(str[i]);
+            else if (str[i] == ')')
+            {
+                if (!s.empty() && s.top() == '(')
+                    s.pop();
+                else
+                {
+                    flag = false;
+                    break;
+                }  
+            }                
+            else if (str[i] == ']')
+            {
+                if(!s.empty() && s.top() == '[')
+                    s.pop();
+                else
+                {
+                    flag = false;
+                    break;
+                }
+            }
+                
+        }
+        if (flag && s.empty())
+            cout << "yes";
+        else
+            cout << "no";
+        cout << '\n';
+    }
+    return 0;
+}
