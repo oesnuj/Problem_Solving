97518c0ea7ed715596a90d890368c6ba310f907c
Junseo Kim <semiwest2@gmail.com>
2024-03-12 22:45:10 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char n;
+    cin >> n;
+    if(n == 'n' || n =='N')
+        cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+    return 0;
+}
