04d1205cd6fdc142336933535ec9184a5367a449
Junseo Kim <semiwest2@gmail.com>
2024-03-19 23:42:01 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << str << "::fan:\n";
+    cout << ":fan::fan::fan:";
+    return 0;
+}
