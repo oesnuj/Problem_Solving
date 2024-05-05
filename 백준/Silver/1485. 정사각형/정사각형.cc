15ca06970cd163c542f68332508d3306ab296241
김준서 Junseo Kim <semiwest2@gmail.com>
2024-05-06 06:27:14 +0900

    [Silver III] Title: 정사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1485. 정사각형/정사각형.cc
+++ 백준/Silver/1485. 정사각형/정사각형.cc
@@ -9,25 +9,16 @@ struct Point
     int x, y;
 };
 
-
 int calcDistance(Point a, Point b)
 {
     return pow(a.x - b.x, 2) + pow(a.y - b.y, 2);
 }
 
-void Sort(vector <Point>& v)
+bool compareInfo(Point &a, Point &b)
 {
-    for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4 - 1 - i; j++)
-        {
-            if (v[j].x > v[j + 1].x)
-                swap(v[j], v[j + 1]);
-
-            else if(v[j].x == v[j + 1].x)
-                if (v[j].y > v[j + 1].y)
-                    swap(v[j], v[j + 1]);
-        }
-    }
+    if(a.x == b.x)
+        return a.y < b.y;
+    return a.x < b.x;
 }
 
 int main() {
@@ -43,7 +34,7 @@ int main() {
         for (int i = 0; i < 4; i++) {
             cin >> v[i].x >> v[i].y;
         }
-        Sort(v);
+        sort(v.begin(), v.end(),compareInfo);
         //2 3
         //0 1
         int s1 = calcDistance(v[0], v[1]); //선분
@@ -52,8 +43,8 @@ int main() {
         int s4 = calcDistance(v[2], v[3]);
 
         int dia1 = calcDistance(v[0], v[3]); //대각선
-        int dia2 = calcDistance(v[1], v[2]); //대각선
-        if (s1 == s2 && s2 == s3 && s3 == s4 && dia1 == dia2)
+        int dia2 = calcDistance(v[1], v[2]);
+        if (s1 == s2 && s2 == s3 && s3 == s4 && dia1 == dia2) //네변의 길이가 같고 대각선의 길이가 같다.
             cout << 1 << '\n';
         else
             cout << 0 << '\n';
