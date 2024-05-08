3247ef91efb9d817808c62e7d1b118fb3b3449a9
김준서 Junseo Kim <semiwest2@gmail.com>
2024-05-08 22:19:06 +0900

    [Silver V] Title: 국회의원 선거, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -1,17 +1,19 @@
 N = int(input())
 dasom = int(input())
 vote = []
-
-for _ in range(N-1):
-    vote.append(int(input()))
-
 cnt = 0
 
-while max(vote, default=0) >= dasom:
-    dasom += 1
-    max_vote = max(vote, default=0)  # 배열에서 최댓값을 찾음
-    max_index = vote.index(max_vote)  # 최댓값의 인덱스를 찾음
-    vote[max_index] -= 1
-    cnt += 1
+for _ in range(N-1):
+  vote.append(int(input()))
 
-print(cnt)
+vote.sort(reverse=True) #내림차순 정렬
+
+if N == 1:
+  print('0')
+else:
+    while vote[0] >= dasom:
+        dasom += 1
+        vote[0] -= 1
+        cnt += 1
+        vote.sort(reverse=True)
+    print(cnt)
