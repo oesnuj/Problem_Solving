e2c5b5958282079d3e565d0263a012d621a76992
김준서 Junseo Kim <semiwest2@gmail.com>
2024-05-12 17:12:32 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    priority_queue <int, vector<int>, greater<>> q;
+
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        long long int x;
+        cin >> x;
+        if (x == 0)
+        {
+            if (!q.empty())
+            {
+                cout << q.top() << '\n';
+                q.pop();
+            }
+            else
+                cout << 0 << '\n';
+        }
+        else
+            q.push(x);
+    }
+
+    return 0;
+}
