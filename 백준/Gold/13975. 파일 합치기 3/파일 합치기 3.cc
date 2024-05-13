5ed10f553cb5294b0a188cc027b5b3d916aa8dbd
김준서 Junseo Kim <semiwest2@gmail.com>
2024-05-14 00:41:48 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 736 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);    cin.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        priority_queue <long long int, vector<long long int>, greater<long long int>> pq;
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            pq.push(x);
+        }
+        long long int result = 0;
+        while (pq.size() > 1)
+        {
+            long long int a = pq.top();
+            pq.pop();
+            long long int b = pq.top();
+            pq.pop();
+            long long int sum = a + b;
+            pq.push(sum);
+            result += sum;
+        }
+        cout << result << '\n';
+    }
+    
+    
+    return 0;
+}
