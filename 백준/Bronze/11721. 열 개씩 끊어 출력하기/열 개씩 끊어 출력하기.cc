ae92cd0119e8950a9dfdc9f33219ff5d0d152175
김준서 Junseo Kim <semiwest2@gmail.com>
2024-05-24 23:40:08 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);    cin.tie(NULL);
+
+    string str;
+    cin >> str;
+    for (int i = 0; i < str.length(); i ++)
+    {
+        if (i != 0 && i % 10 == 0)
+            cout << '\n';
+        cout << str[i];
+    }
+    return 0;
+}
