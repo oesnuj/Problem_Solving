88b8144002180536269207a4448c6dd1989232b5
김준서 Junseo Kim <semiwest2@gmail.com>
2024-06-03 00:35:40 +0900

    [Bronze V] Title: 세제곱의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/28701. 세제곱의 합/세제곱의 합.cc
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.cc
@@ -5,12 +5,10 @@ int main() {
     int n;
     cin >> n;
     int a = 0;
-    int b = 0;
     for (int i = 1; i <= n; i++)
     {
         a += i;
-        b += (i * i * i);
     }
-    cout << a << '\n' << a * a << '\n' << b;
+    cout << a << '\n' << a * a << '\n' << a * a;
     return 0;
 }
