a87fd4cf2a9cfce76b1cd057975910e7eb2784c5
김준서 Junseo Kim <semiwest2@gmail.com>
2024-06-03 10:30:25 +0900

    [Silver IV] Title: ZOAC 3, Time: 100 ms, Memory: 9612 KB -BaekjoonHub

--- 백준/Silver/20436. ZOAC 3/ZOAC 3.js
+++ 백준/Silver/20436. ZOAC 3/ZOAC 3.js
@@ -1,55 +1,37 @@
-const fs = require('fs')
+const fs = require('fs');
 const filepath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filepath).toString().trim().split('\n');
 
-const current = input[0].trim().split(' ');
+const [leftStart, rightStart] = input[0].trim().split(' ');
 const str = input[1];
 
-const left = new Map();
-const right = new Map();
+const left = new Map([
+    ['q', { x: 0, y: 0 }], ['w', { x: 0, y: 1 }], ['e', { x: 0, y: 2 }], ['r', { x: 0, y: 3 }], ['t', { x: 0, y: 4 }],
+    ['a', { x: 1, y: 0 }], ['s', { x: 1, y: 1 }], ['d', { x: 1, y: 2 }], ['f', { x: 1, y: 3 }], ['g', { x: 1, y: 4 }],
+    ['z', { x: 2, y: 0 }], ['x', { x: 2, y: 1 }], ['c', { x: 2, y: 2 }], ['v', { x: 2, y: 3 }]
+]); 
 
-// left 맵에 값 추가
-left.set('q', { x: 0, y: 0 });
-left.set('w', { x: 0, y: 1 });
-left.set('e', { x: 0, y: 2 });
-left.set('r', { x: 0, y: 3 });
-left.set('t', { x: 0, y: 4 });
-left.set('a', { x: 1, y: 0 });
-left.set('s', { x: 1, y: 1 });
-left.set('d', { x: 1, y: 2 });
-left.set('f', { x: 1, y: 3 });
-left.set('g', { x: 1, y: 4 });
-left.set('z', { x: 2, y: 0 });
-left.set('x', { x: 2, y: 1 });
-left.set('c', { x: 2, y: 2 });
-left.set('v', { x: 2, y: 3 });
+const right = new Map([
+    ['y', { x: 0, y: 1 }], ['u', { x: 0, y: 2 }], ['i', { x: 0, y: 3 }], ['o', { x: 0, y: 4 }], ['p', { x: 0, y: 5 }],
+    ['h', { x: 1, y: 1 }], ['j', { x: 1, y: 2 }], ['k', { x: 1, y: 3 }], ['l', { x: 1, y: 4 }],
+    ['b', { x: 2, y: 0 }], ['n', { x: 2, y: 1 }], ['m', { x: 2, y: 2 }]
+]);
 
-// right 맵에 값 추가
-right.set('y', { x: 0, y: 1 });
-right.set('u', { x: 0, y: 2 });
-right.set('i', { x: 0, y: 3 });
-right.set('o', { x: 0, y: 4 });
-right.set('p', { x: 0, y: 5 });
-right.set('h', { x: 1, y: 1 });
-right.set('j', { x: 1, y: 2 });
-right.set('k', { x: 1, y: 3 });
-right.set('l', { x: 1, y: 4 });
-right.set('b', { x: 2, y: 0 });
-right.set('n', { x: 2, y: 1 });
-right.set('m', { x: 2, y: 2 });
-
-let currentLeft = left.get(current[0]);
-let currentRight = right.get(current[1]);
+let currentLeft = left.get(leftStart);
+let currentRight = right.get(rightStart);
 
 let count = 0;
-for (let char of str) {
+for (const char of str) {
     count++;
     if (left.has(char)) {
-        count += Math.abs(currentLeft.x - left.get(char).x) + Math.abs(currentLeft.y - left.get(char).y);
-        currentLeft = left.get(char);
+        const newLeft = left.get(char);
+        count += Math.abs(currentLeft.x - newLeft.x) + Math.abs(currentLeft.y - newLeft.y);
+        currentLeft = newLeft;
     } else if (right.has(char)) {
-        count += Math.abs(currentRight.x - right.get(char).x) + Math.abs(currentRight.y - right.get(char).y);
-        currentRight = right.get(char);
+        const newRight = right.get(char);
+        count += Math.abs(currentRight.x - newRight.x) + Math.abs(currentRight.y - newRight.y);
+        currentRight = newRight;
     }
 }
+
 console.log(count);
