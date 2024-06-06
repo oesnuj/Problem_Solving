382b9a4d0f0573304f584629c3423ae2b0106c3a
김준서 Junseo Kim <semiwest2@gmail.com>
2024-06-07 00:29:22 +0900

    [Silver III] Title: 파일 정리, Time: 536 ms, Memory: 68224 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.js
+++ 백준/Silver/20291. 파일 정리/파일 정리.js
@@ -1,30 +1,23 @@
 const fs = require('fs');
 const filepath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filepath).toString().trim().split('\n');
+//입력 데이터 정리
 const n = +input.shift();
-const files = input.map(x => {
-    const parts = x.trim().split('.');
-    const name = parts.slice(0, -1).join('.');
-    const extension = parts[parts.length - 1];
-    return { name, extension };
+const files = input.map(x => x.trim().split('.').pop());
+
+let map = new Map();
+
+files.forEach(extension => {
+    map.set(extension, (map.get(extension) || 0) + 1);
 });
 
-let map =new Map();
-
-for(let i = 0; i < n; i++){
-        if(map.has(files[i].extension))
-            map.set(files[i].extension, map.get(files[i].extension) + 1);
-        else
-            map.set(files[i].extension, 1)
-    }
-const mapToArray = [...map];
-mapToArray.sort();
+const sortedEntries = [...map.entries()].sort((a, b) => a[0].localeCompare(b[0]));
 
 printResult();
 
 function printResult() {
     let result = '';
-    for (const [key, value] of mapToArray) {
+    for (const [key, value] of sortedEntries) {
       result += `${key} ${value}\n`;
     }
     console.log(result.trim());
