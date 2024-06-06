fde9f9bbf4da406c9a3593c9c409ce1675b4b9f2
김준서 Junseo Kim <semiwest2@gmail.com>
2024-06-07 00:35:40 +0900

    [Silver III] Title: 파일 정리, Time: 392 ms, Memory: 68644 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.js
+++ 백준/Silver/20291. 파일 정리/파일 정리.js
@@ -3,22 +3,18 @@ const filepath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filepath).toString().trim().split('\n');
 //입력 데이터 정리
 const n = +input.shift();
-const files = input.map(x => x.trim().split('.').pop());
 
-let map = new Map();
+let myMap = new Map();
 
-files.forEach(extension => {
-    map.set(extension, (map.get(extension) || 0) + 1);
-});
-
-const sortedEntries = [...map.entries()].sort((a, b) => a[0].localeCompare(b[0]));
-
-printResult();
-
-function printResult() {
-    let result = '';
-    for (const [key, value] of sortedEntries) {
-      result += `${key} ${value}\n`;
-    }
-    console.log(result.trim());
+for (let i = 0; i < n; i++) {
+    let x = input[i].split('.')[1];
+    myMap.set(x, myMap.has(x) ? myMap.get(x) + 1 : 1);
   }
+
+console.log(
+  [...myMap]
+    .map((e) => e.join(" "))
+    .sort()
+    .join("\n")
+    .trim()
+);
