16602e8c43a598714b5ab38258f045ba9085dc17
김준서 Junseo Kim <semiwest2@gmail.com>
2024-06-13 09:27:24 +0900

    Update 인공지능 청소기.cc

--- goormlevel/43068/1. 인공지능 청소기/인공지능 청소기.cc
+++ goormlevel/43068/1. 인공지능 청소기/인공지능 청소기.cc
@@ -5,18 +5,13 @@ using namespace std;
 int main() {
 	int T;
 	cin >> T;
-	while(T--)
-	{
-		int x, y;
-		int n;
+	while(T--){
+		int x, y, n;
 		cin >> x >> y >> n;
 		int targetSum = abs(x - 0) + abs(y - 0);
-		if(targetSum > n) //가능한 거리보다 먼 좌표인 경우
-			cout << "NO\n";
-		else if((targetSum % 2 == 0 && n % 2 == 0) || (targetSum % 2 != 0 && n % 2 != 0))
+		if(targetSum <= n && ((targetSum % 2 == 0 && n % 2 == 0) || (targetSum % 2 != 0 && n % 2 != 0))) //가능한 경우(가능한 거리이면서 짝수시간, 홀수시간으로 도달가능한 거리인지 판단)
 			cout << "YES\n";
-		else//가능은 하지만 홀수시간으로만 도달가능한 경우
+		else
 			cout << "NO\n";
 	}
 	return 0;
-}
