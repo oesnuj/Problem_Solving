9312e7ed4b0688545e9fddf20353cecd950f6186
김준서 Junseo Kim <semiwest2@gmail.com>
2024-06-13 22:36:53 +0900

    [goormlevel] Title: [KOI 2017] 딱지놀이, Time: 0, Memory: 0, Difficulty: 0 -BaekjoonHub

--- goormlevel/48130/1. ［KOI 2017］ 딱지놀이/［KOI 2017］ 딱지놀이.cc
+++ goormlevel/48130/1. ［KOI 2017］ 딱지놀이/［KOI 2017］ 딱지놀이.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 void input(int &num, vector <int> &v);
 char compare(vector <int> &a, vector <int> &b);
-void printWinner(char &winner);
+void printWinner(char winner);
 
 int main() {
 	int n;
@@ -19,43 +19,33 @@ int main() {
 		input(numA, a);
 		cin >> numB;
 		input(numB, b);
-		char winner = compare(a, b);
-		printWinner(winner);
+		printWinner(compare(a, b));
 	}
 	cout << endl;
 	return 0;
 }
 
-void input(int &num, vector <int> &v) //입력 숫자에 맞게 딱지 타입과 그 갯수를 name 벡터에 저장한다.
+void input(int &num, vector <int> &v)
 {
+	int type;
 	for(int i = 0; i<num; i++)
 	{
-		int type;
 		cin >> type;
 		v[type-1]++;
 	}
 }
 
-char compare(vector <int> &a, vector <int> &b) //a, b 비교 후 이긴사람을 출력해준다.
+char compare(vector <int> &a, vector <int> &b) 
 {
 	for(int i = 3; i>=0; i--)
 	{
-		if(a[i]>b[i])
-			return 'A';
-		else if(a[i]<b[i])
-			return 'B';
-		else
-			if(i == 0)
-				return 'D';
+		if(a[i]>b[i]) return 'A';
+		else if(a[i]<b[i]) return 'B';
 	}
+	return 'D';
 }
 
-void printWinner(char &winner)
+void printWinner(char winner)
 {
-	if(winner == 'A')
-		cout << "A\n";
-	else if(winner == 'B')
-		cout << "B\n";
-	else if(winner == 'D')
-		cout << "D\n";
+	cout << winner << '\n';
 }
