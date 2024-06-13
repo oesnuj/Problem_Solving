485d9e3c4bcee9487fe2607a8c2aa81710fd9439
김준서 Junseo Kim <semiwest2@gmail.com>
2024-06-13 22:01:05 +0900

    [goormlevel] Title: 소금물의 농도 구하기, Time: 0.00 ms, Memory: 3.54 MB, Difficulty: 1 -BaekjoonHub

--- goormlevel/194193/1. 소금물의 농도 구하기/소금물의 농도 구하기.cc
+++ goormlevel/194193/1. 소금물의 농도 구하기/소금물의 농도 구하기.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <cstdio>
 using namespace std;
 int main() {
 	int n, m;
@@ -7,9 +8,8 @@ int main() {
 	
 	double salt = n * 0.07; //소금양 추출
 	double water = n + m; //젠체 소금물 g구하기
-	double concentration = (salt / water) * 100;
-	cout << fixed;
-	cout.precision(2);
-	cout << floor(concentration * 100) / 100.0 << endl;
+	 double concentration = (salt / water) * 100.0;
+	concentration = floor(concentration * 100) / 100.0;
+	printf("%.2f", concentration);
 	return 0;
-}
+} 
