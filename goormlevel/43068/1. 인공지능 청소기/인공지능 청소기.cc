0c1783c7323624b2e396f8bed4c4a0eef04d6f61
김준서 Junseo Kim <semiwest2@gmail.com>
2024-06-13 22:21:35 +0900

    [goormlevel] Title: 인공지능 청소기, Time: 0, Memory: 0, Difficulty: 1 -BaekjoonHub

--- goormlevel/43068/1. 인공지능 청소기/인공지능 청소기.cc
+++ goormlevel/43068/1. 인공지능 청소기/인공지능 청소기.cc
@@ -9,9 +9,10 @@ int main() {
 		int x, y, n;
 		cin >> x >> y >> n;
 		int targetSum = abs(x - 0) + abs(y - 0);
-		if(targetSum <= n && ((targetSum % 2 == 0 && n % 2 == 0) || (targetSum % 2 != 0 && n % 2 != 0))) //가능한 경우(가능한 거리이면서 짝수시간, 홀수시간으로 도달가능한 거리인지 판단)
+		if(targetSum <= n && (targetSum % 2 == n % 2)) //가능한 경우(가능한 거리이면서 짝수시간, 홀수시간으로 도달가능한 거리인지 판단)
 			cout << "YES\n";
 		else
 			cout << "NO\n";
 	}
 	return 0;
+}
