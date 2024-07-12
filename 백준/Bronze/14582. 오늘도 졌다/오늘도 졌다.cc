e02a957c2bef43ed0a8e79bd2df95f4b0609c251
김준서 Junseo Kim <semiwest2@gmail.com>
2024-07-12 17:20:00 +0900

    [Bronze I] Title: 오늘도 졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14582. 오늘도 졌다/오늘도 졌다.cc
+++ 백준/Bronze/14582. 오늘도 졌다/오늘도 졌다.cc
@@ -1,31 +1,27 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    int w[9], s[9];
-    for (auto& i : w) {
-        cin >> i;
-    }
-    for (auto& i : s) {
-        cin >> i;
-    }
-
+	int w[9], s[9];
+	for (auto& i : w) {
+		cin >> i;
+	}
+	for (auto& i : s) {
+		cin >> i;
+	}
+	//울림이 이기던 적이 있는가?
     int sumW = 0, sumS = 0;
-    bool leading = false;
-
-    for (int i = 0; i < 9; i++) {
-        sumW += w[i];
-        if (sumW > sumS) {
-            leading = true;
-        }
-        sumS += s[i];
-    }
-
-    if (leading)
-        cout << "Yes";
-    else
-        cout << "No";
-
-    return 0;
+	for (int i = 0; i < 9; i++) {
+		sumW += w[i];
+		if (sumW > sumS) {
+			cout << "Yes";
+			return 0;
+		}
+		sumS += s[i];
+	}
+    if (sumW > sumS) {
+			cout << "Yes";
+			return 0;
+		}
+	cout << "No";
 }
