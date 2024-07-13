fd5f725689fe16119a839bd52c443ebc86ca3eb8
김준서 Junseo Kim <semiwest2@gmail.com>
2024-07-13 11:02:23 +0900

    [Silver III] Title: 어린 왕자, Time: 8 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float calcDistance(int& x1, int& y1, int& x2, int& y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+int main() {
+    int T, x1, y1, x2, y2, cx, cy, r, n;
+    float distance = 0;
+    cin >> T;
+    while (T--) {
+        int sum = 0;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cin >> n;
+        while (n--) {
+            cin >> cx >> cy >> r;
+            bool start_in_circle = calcDistance(x1, y1, cx, cy) <= r;
+            bool end_in_circle = calcDistance(x2, y2, cx, cy) <= r;
+            if (start_in_circle != end_in_circle) sum++;
+        }
+        cout << sum << '\n';
+    }
+    return 0;
+}
