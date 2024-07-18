30782c7eeefdc8cba4d9059e9cca857ffa7293b0
김준서 Junseo Kim <semiwest2@gmail.com>
2024-07-18 19:34:17 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-bool check(vector <int> &v, long long length, int n){
+bool check(vector <int>& v, int n, long long length);
+int binary_serach(vector <int>& v, int n);
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    int k, n;
+    cin >> k >> n;
+    vector <int> trees(k);
+    for (auto& i : trees) {
+        cin >> i;
+    }
+    cout << binary_serach(trees, n);
+    return 0;
+}
+
+bool check(vector <int>& v, int n, long long length) {
     int cnt = 0;
     for (const auto& i : v) {
         cnt += i / length;
@@ -10,24 +26,12 @@ bool check(vector <int> &v, long long length, int n){
     return cnt >= n;
 }
 
-int binary_serach(vector <int> &v, int n) {
-    long long lo = 1, hi = 2147483648;
-    while(lo + 1 < hi) {
+int binary_serach(vector <int>& v, int n) {
+    long long lo = 1, hi = 2147483648; //자를 수 있는 랜선의 범위
+    while (lo + 1 < hi) {
         long long mid = (lo + hi) / 2;
-        if (check(v, mid, n)) lo = mid;
-        else
-            hi = mid;
+        if (check(v, n, mid)) lo = mid;
+        else hi = mid;
     }
     return lo;
-}
-
-int main() {
-    int k, n;
-    cin >> k >> n;
-    vector <int> trees(k);
-    for (auto &i : trees) {
-        cin >> i;
-    }
-    cout << binary_serach(trees, n);
-    return 0;
 }
