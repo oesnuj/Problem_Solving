b54d6a222b0d716f5ee0131c6789b2845c360930
김준서 Junseo Kim <semiwest2@gmail.com>
2024-07-23 23:55:28 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n--) {
+        int k;
+        cin >> k;
+        while (k--) {
+            cout << "=";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
