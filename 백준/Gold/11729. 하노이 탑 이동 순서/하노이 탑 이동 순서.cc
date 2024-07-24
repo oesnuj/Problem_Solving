dade793ee42d22822cd06cb2e3632a1b9f52b2e0
김준서 Junseo Kim <semiwest2@gmail.com>
2024-07-25 05:04:16 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 116 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,25 +1,33 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 using namespace std;
 
-void recursion(int n, int from, int temp, int to)
-{
-	if (n == 1)
-	{
-		cout << from << " " << to << "\n";
-		return;
-	}
-	recursion(n - 1, from, to, temp);
-	cout << from << " " << to << "\n";
-	recursion(n - 1, temp, from, to);
+void RecurMoveDisks(int n, int from, int temp, int to) {
+    if (n == 0) return;
+    //n-1개의 원판으로 temp로 옮긴다.
+    RecurMoveDisks(n - 1, from, to, temp);
+
+    //남은 1개의 원판으로 to로 옮긴다. <- 이때 옮기는 과정 출력
+    cout << from << " " << to << "\n";
+
+    //temp에 있던 n-1개의 원판을 n-1개로 옮긴다.
+    RecurMoveDisks(n - 1, temp, from, to);
 }
 
-int main()
-{
-	ios::sync_with_stdio(0);  cin.tie(0);
-	int n; cin >> n;
-    
-	//하노이탑 공식 2의 n 제곱 -1
-	cout << (1 << n) - 1 << '\n'; //쉬프트 연산으로 1을 만큼 옮겨 2의 n승을 구한다.
-	recursion(n, 1, 2, 3);
-	return 0;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int n;
+    cin >> n;
+
+    //2의 n제곱 -1 출력하기
+    string a = to_string(pow(2, n));
+    int x = a.find('.');
+    a = a.substr(0, x);
+    a[a.length() - 1] -= 1;
+    cout << a << "\n";
+
+    //조건에 맞는 경우에만 재귀를 돌린다.
+    if (n <= 20)  RecurMoveDisks(n, 1, 2, 3);
+    return 0;
 }
