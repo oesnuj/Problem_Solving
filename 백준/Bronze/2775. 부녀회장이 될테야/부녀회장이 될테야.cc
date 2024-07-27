7de7eb998367fb4de1309113d43c20d8cfae453f
김준서 Junseo Kim <semiwest2@gmail.com>
2024-07-28 03:37:59 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -1,33 +1,28 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int calc(int k, int n) {
-    //0층이라면 n이 거주자수
-    if (k == 0) 
-    { 
-        return n;
-    }
+vector<vector<int>> dp;
 
-    else //0층이 아니라면, 해당 아래층의 합
-    {
-        int sum = 0;
-        for (int i = n; i > 0; i--) {
-            sum += calc(k - 1, i);
-        }
-        return sum;
+int calc(int k, int n) {
+    if (k == 0) return n; // 기본 케이스
+    if (dp[k][n] != -1) return dp[k][n]; // 이미 계산된 값이면 반환
+
+    int sum = 0;
+    for (int i = n; i > 0; i--) {
+        sum += calc(k - 1, i); // 재귀적으로 호출하여 하위 문제 해결
     }
+    return dp[k][n] = sum; // 계산된 값을 저장하고 반환
 }
 
-
-int main() 
+int main()
 {
     int T, k, n;
     cin >> T;
     while (T--) {
         cin >> k >> n;
-        cout << calc(k, n) << "\n";
+        dp = vector<vector<int>>(k + 1, vector<int>(n + 1, -1)); // DP 테이블 초기화
+        cout << calc(k, n) << "\n"; // 결과 출력
     }
-
     return 0;
 }
