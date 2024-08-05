cc6edc889f2c684e2905d169e4f367ab50eeb12c
김준서 Junseo Kim <semiwest2@gmail.com>
2024-08-06 04:52:46 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 300 ms, Memory: 54348 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
@@ -8,22 +8,21 @@ const testList = input.slice(1 + N);
 
 // 이름을 키로 하고 번호를 값으로 하는 단일 Map 객체 생성
 const pokemonMap = new Map();
-
 pokemonList.forEach((pokemon, index) => {
     const number = index + 1;
     pokemonMap.set(pokemon, number);
 });
 
-// 테스트 리스트의 Pokémon을 조회하고 출력
-const output = testList.map(test => {
+
+let result = '';
+testList.forEach(test => {
     const num = Number(test);
     if (!isNaN(num)) {
         // 테스트 값이 번호인 경우
-        return pokemonList[num - 1]; // 번호에 해당하는 Pokémon 이름 출력
+        result += pokemonList[num - 1] + '\n'; // 번호에 해당하는 Pokémon 이름 추가
     } else {
         // 테스트 값이 이름인 경우
-        return pokemonMap.get(test); // 이름에 해당하는 Pokémon 번호 출력
+        result += pokemonMap.get(test) + '\n'; // 이름에 해당하는 Pokémon 번호 추가
     }
-}).join('\n');
-
-console.log(output);
+});
+console.log(result);
