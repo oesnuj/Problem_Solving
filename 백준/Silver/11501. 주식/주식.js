9ec6b67e8ad0ebb054931004278ed0bd0645d8cb
김준서 Junseo Kim <semiwest2@gmail.com>
2024-08-10 20:17:12 +0900

    [Silver II] Title: 주식, Time: 2024 ms, Memory: 443168 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.js
+++ 백준/Silver/11501. 주식/주식.js
@@ -1,20 +1,17 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-main();
 
-function main(){
-    let answer ="";
-    for(let i = 2; i < input.length; i += 2) {
-        answer += calc(i) + "\n";
-    }
-    console.log(answer);
+let answer ="";
+for(let i = 2; i < input.length; i += 2) {
+    answer += calc(i) + "\n";
 }
+console.log(answer);
 
 function calc(i){
-    let stockFlow = input[i].trim().split(' ').map(e => parseInt(e));
+    let stockFlow = input[i].split(' ').map(Number);
     let profit = 0;
     let maxPrice = 0;
     for(let j = stockFlow.length - 1; j >= 0; j--) {
-        if (stockFlow[j] >= maxPrice) {
+        if (stockFlow[j] > maxPrice) {
             maxPrice = stockFlow[j];
         }
         profit += maxPrice - stockFlow[j];
