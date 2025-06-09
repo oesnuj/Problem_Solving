d08c8a880ab052e939c6dd648a1b9d46520a37fd
김준서 Junseo Kim <semiwest2@gmail.com>
2025-06-09 18:35:24 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.75 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,20 +1,12 @@
-function dateParser(date){
+function convertToDays(date){
     const [year, month, day] = date.split('.').map(Number);
-    return {year , month, day};
+    return year * 12 * 28 + month * 28 + day;
 }
-
-function isExpired(today, base) {
-  return (
-      base.year < today.year ||
-      (base.year === today.year && base.month < today.month) ||
-      (base.year === today.year && base.month === today.month && base.day <= today.day)
-  );
-}
-
 
 function solution(today, terms, privacies) {
     let answer= [];
     const validityByTerms = new Map();
+    const todayDate = convertToDays(today);
 
     terms.forEach(t => {
         const [key, value] = t.split(' ');
@@ -23,16 +15,10 @@ function solution(today, terms, privacies) {
     
     privacies.forEach((p, index) => {
        const [date, term] = p.split(' ');
-        let baseDate = dateParser(date);
-        const todayDate = dateParser(today);
         const validity = validityByTerms.get(term);
-        const totalMonths = baseDate.month + validity;
-        
-        baseDate.year += Math.floor((totalMonths - 1) / 12);
-        baseDate.month = ((totalMonths - 1) % 12) + 1;
-        
-        if (isExpired(todayDate, baseDate)) {
-          answer.push(index + 1);
+        const expiryDate = convertToDays(date) + validity * 28;
+        if(expiryDate <= todayDate){
+            answer.push(index + 1)
         }
     })
     return answer;
