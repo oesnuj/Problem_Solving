0b96ce19bbed13c307af5f2d00a4f697a4f9c9b7
김준서 Junseo Kim <semiwest2@gmail.com>
2025-06-17 23:27:26 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -1,10 +1,9 @@
 #include <iostream>
-
 using namespace std;
 
 long long p[100];
 
-long long recursion(int n) {
+long long dp(int n) {
 	p[1] = p[2] = p[3] = 1;
 	for(int i = 4; i <= n; i++) {
 		p[i] = p[i - 2] + p[i - 3];
@@ -19,7 +18,7 @@ int main() {
 	while (T--) {
 		int n;
 		cin >> n;
-		cout << recursion(n) << '\n';
+		cout << dp(n) << '\n';
 	}
 	return 0;
 }
