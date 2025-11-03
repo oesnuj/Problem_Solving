865272cafa300f5b3ad7c15c28e46ad703f14c1d
김준서 Junseo Kim <semiwest2@gmail.com>
2025-11-03 11:48:14 +0900

    [level 1] Title: 모의고사, Time: 2.56 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -2,19 +2,20 @@ function solution(answers) {
     const person1 = [1, 2, 3, 4, 5];
     const person2 = [2, 1, 2, 3, 2, 4, 2, 5];
     const person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    const scores = {
-        1 : 0,
-        2 : 0,
-        3 : 0
-    }
-    answers.forEach((e, i) => {
-        if(person1[i % person1.length] === e) scores[1]++;
-        if(person2[i % person2.length] === e) scores[2]++;
-        if(person3[i % person3.length] === e) scores[3]++;
-    })
-    const max = Math.max(...Object.values(scores));
-    const result = Object.keys(scores)
-        .filter(key => scores[key] === max).map(Number);
+    const scores = [0, 0, 0]
+    const result = [];
     
+    answers.forEach((e, i) => {
+        if(person1[i % person1.length] === e) scores[0]++;
+        if(person2[i % person2.length] === e) scores[1]++;
+        if(person3[i % person3.length] === e) scores[2]++;
+    })
+    const max = Math.max(...scores);
+    
+    scores.forEach((score, idx) => {
+        if (score === max) {
+            result.push(idx + 1);
+        }
+    });
     return result;
 }
