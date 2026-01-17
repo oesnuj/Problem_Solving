a27ff5e2b8ae2c05cae4f876356958a08999ed87
김준서 Junseo Kim <semiwest2@gmail.com>
2026-01-17 18:14:21 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1336 ms, Memory: 93188 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.js
@@ -0,0 +1,32 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? 0 : 'input.txt', 'utf8')
+  .trim()
+  .split('\n');
+
+const [N, M, R] = input[0].split(' ').map(Number);
+const adjList = Array.from({ length: N + 1 }, () => []);
+const visited = Array(N + 1).fill(0);
+const queue = [];
+for (let i = 1; i <= M; i++) {
+  const [u, v] = input[i].split(' ').map(Number);
+  adjList[u].push(v);
+  adjList[v].push(u);
+}
+
+adjList.forEach((list) => list.sort((a, b) => a - b));
+
+let order = 1;
+queue.push(R);
+visited[R] = order++;
+while (queue.length) {
+  const node = queue.shift();
+
+  adjList[node].forEach((u) => {
+    if (!visited[u]) {
+      queue.push(u);
+      visited[u] = order++;
+    }
+  });
+}
+
+console.log(visited.slice(1).join('\n'));
