92746b6940eedbed8899087da3eaca0c855bdefd
김준서 Junseo Kim <semiwest2@gmail.com>
2026-01-18 02:01:29 +0900

    [level 2] Title: 타겟 넘버, Time: 40.14 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+    let count = 0;
+    function recursion(i, sum){
+        if(i === numbers.length){
+           if(sum === target) count++;
+            return;
+       }
+        recursion(i+1, sum + numbers[i])
+        recursion(i+1, sum - numbers[i])
+        return count;
+    }
+    
+    recursion(0, 0);
+    return count;
+}
+
+
