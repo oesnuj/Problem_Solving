df55f5ca6e75fb06d79ea823b9372dcbb4f5200a
김준서 Junseo Kim <semiwest2@gmail.com>
2026-01-17 17:30:28 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 668 ms, Memory: 91644 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.js
@@ -0,0 +1,33 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? 0 : 'input.txt', 'utf8')
+  .trim()
+  .split('\n');
+const [N, M, R] = input[0].split(' ').map(Number);
+const adjList = Array.from({ length: N + 1 }, () => []);
+const visited = Array(N + 1).fill(0);
+
+for (let i = 1; i <= M; i++) {
+  const [u, v] = input[i].split(' ').map(Number);
+  adjList[u].push(v);
+  adjList[v].push(u);
+}
+
+for (let i = 1; i <= N; i++) {
+  adjList[i].sort((a, b) => a - b);
+}
+
+let order = 1;
+
+function dfs(node) {
+  visited[node] = order++;
+
+  for (const next of adjList[node]) {
+    if (!visited[next]) {
+      dfs(next);
+    }
+  }
+}
+
+dfs(R);
+
+console.log(visited.slice(1).join('\n'));
