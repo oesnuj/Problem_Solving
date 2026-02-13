236ac6476c191554bfecd5560259d17e4b39b810
김준서 Junseo Kim <semiwest2@gmail.com>
2026-02-14 01:46:02 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 328 ms, Memory: 11160 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.js
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.js
@@ -4,41 +4,41 @@ const input = require('fs')
   .split('\n');
 
 const N = +input[0];
-const sliced_input = input.slice(1);
-const eggs = sliced_input.map((e) => e.trim().split(' ').map(Number));
+const eggs = input.slice(1).map((e) => e.split(' ').map(Number));
 
 let max = 0;
+let brokenCnt = 0;
 recursion(0);
 console.log(max);
 
 function recursion(k) {
+  // 모든 턴 종료
   if (k === N) {
-    const broken = eggs.filter((e) => e[0] <= 0).length;
-    max = Math.max(max, broken);
+    max = Math.max(max, brokenCnt);
     return;
   }
 
-  if (eggs[k][0] <= 0) {
+  // 현재 계란이 깨졌거나 더 이상 충돌 불가능
+  if (eggs[k][0] <= 0 || brokenCnt === N - 1) {
     recursion(k + 1);
     return;
   }
 
-  let targets = [];
   for (let i = 0; i < N; i++) {
-    if (i !== k && eggs[i][0] > 0) targets.push(i);
-  }
+    if (i === k || eggs[i][0] <= 0) continue;
 
-  if (targets.length === 0) {
-    recursion(k + 1);
-    return;
-  }
-
-  for (const i of targets) {
+    // 충돌
     eggs[k][0] -= eggs[i][1];
     eggs[i][0] -= eggs[k][1];
+    let add = 0;
+    if (eggs[k][0] <= 0) add++;
+    if (eggs[i][0] <= 0) add++;
+    brokenCnt += add;
 
     recursion(k + 1);
 
+    // 복구
+    brokenCnt -= add;
     eggs[k][0] += eggs[i][1];
     eggs[i][0] += eggs[k][1];
   }
