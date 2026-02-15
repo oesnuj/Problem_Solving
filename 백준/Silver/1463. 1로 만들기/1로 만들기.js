223295cd4050b9d204a7c7f7999385fd20c76be3
김준서 Junseo Kim <semiwest2@gmail.com>
2026-02-16 01:34:54 +0900

    [Silver III] Title: 1로 만들기, Time: 156 ms, Memory: 17452 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,16 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? 0 : 'input.txt', 'utf8')
+  .trim()
+  .split('\n');
+
+const N = +input[0];
+const dp = Array(N + 1);
+dp[1] = 0;
+
+for (let i = 2; i <= N; i++) {
+  dp[i] = dp[i - 1] + 1;
+  if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+  if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+}
+
+console.log(dp[N]);
