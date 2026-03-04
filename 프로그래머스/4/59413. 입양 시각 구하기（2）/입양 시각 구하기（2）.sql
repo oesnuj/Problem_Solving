a8ae1f6cbbd50067439beb4e2c34788695b85b32
김준서 Junseo Kim <semiwest2@gmail.com>
2026-03-05 02:00:00 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,14 +1,7 @@
-WITH RECURSIVE hours AS (
-    SELECT 0 AS hour
-    UNION ALL
-    SELECT hour + 1
-    FROM hours
-    WHERE hour < 23
-)
-SELECT 
-    hs.hour AS HOUR,
-    COUNT(ANIMAL_ID) as COUNT
-FROM ANIMAL_OUTS ao
-RIGHT JOIN hours hs ON HOUR(ao.DATETIME) = hs.hour
-GROUP BY hs.hour
-ORDER BY hs.hour;
+SET @hour := -1;
+SELECT @hour := @hour + 1 AS HOUR,
+    (SELECT COUNT(*) 
+    FROM ANIMAL_OUTS 
+    WHERE HOUR(datetime) = @hour) as COUNT
+FROM ANIMAL_OUTS
+WHERE @hour < 23;
