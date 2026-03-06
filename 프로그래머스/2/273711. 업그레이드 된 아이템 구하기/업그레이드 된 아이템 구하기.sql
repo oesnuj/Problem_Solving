56d16913bff1d5e6f00bc1dfa41b977b31fd14d0
김준서 Junseo Kim <semiwest2@gmail.com>
2026-03-06 15:43:42 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,16 @@
+# 희귀도가 RARE인 ID가 부모 ITEM에 있다면 그때 ITEM_ID를 기반으로 전부 출력
+
+
+
+
+SELECT 
+    ii.ITEM_ID, ii.ITEM_NAME, ii.RARITY
+FROM ITEM_TREE it
+JOIN ITEM_INFO ii ON it.ITEM_ID = ii.ITEM_ID
+WHERE PARENT_ITEM_ID IN (
+    SELECT 
+        ITEM_ID
+    FROM ITEM_INFO
+    WHERE RARITY = 'RARE'
+)
+ORDER BY ii.ITEM_ID DESC;
