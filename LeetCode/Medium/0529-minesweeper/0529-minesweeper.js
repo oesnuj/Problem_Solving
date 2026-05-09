9fc5ade59dbfef139681d8d013096601360e844b
김준서 Junseo Kim <semiwest2@gmail.com>
2026-05-09 14:05:09 +0900

    Time: 14 ms (33.82%), Space: 62.7 MB (58.82%) - LeetHub

--- LeetCode/Medium/0529-minesweeper/0529-minesweeper.js
+++ LeetCode/Medium/0529-minesweeper/0529-minesweeper.js
@@ -1,56 +1,49 @@
 function updateBoard(board, click) {
     const [sr, sc] = click;
 
-    if (board[sr][sc] === 'M') {
-        board[sr][sc] = 'X';
-        return board;
-    }
-
-    reveal(board, sr, sc);
-
-    return board;
-}
-
-const dirs = [
-    [-1, -1], [-1, 0], [-1, 1],
-    [ 0, -1],          [ 0, 1],
-    [ 1, -1], [ 1, 0], [ 1, 1],
-];
-
-function inRange(board, r, c) {
     const m = board.length;
     const n = board[0].length;
-    return 0 <= r && r < m && 0 <= c && c < n;
-}
 
-function countMines(board, r, c) {
-    let cnt = 0;
+    const dirs = [
+        [-1, -1], [-1, 0], [-1, 1],
+        [ 0, -1],          [ 0, 1],
+        [ 1, -1], [ 1, 0], [ 1, 1],
+    ];
 
-    for (const [dr, dc] of dirs) {
-        const nr = r + dr;
-        const nc = c + dc;
-
-        if (!inRange(board, nr, nc)) continue;
-        if (board[nr][nc] === 'M') cnt++;
+    function inRange(r, c) {
+        return r >= 0 && r < m && c >= 0 && c < n;
     }
 
-    return cnt;
-}
+    function dfs(r, c) {
+        if (!inRange(r, c) || board[r][c] !== 'E') return;
 
-function reveal(board, r, c) {
-    if (!inRange(board, r, c)) return;
-    if (board[r][c] !== 'E') return;
+        let cnt = 0;
 
-    const cnt = countMines(board, r, c);
+        for (const [dr, dc] of dirs) {
+            const nr = r + dr;
+            const nc = c + dc;
 
-    if (cnt > 0) {
-        board[r][c] = String(cnt);
-        return;
+            if (!inRange(nr, nc)) continue;
+            if (board[nr][nc] === 'M') cnt++;
+        }
+
+        if (cnt > 0) {
+            board[r][c] = String(cnt);
+            return;
+        }
+
+        board[r][c] = 'B';
+
+        for (const [dr, dc] of dirs) {
+            dfs(r + dr, c + dc);
+        }
     }
 
-    board[r][c] = 'B';
-
-    for (const [dr, dc] of dirs) {
-        reveal(board, r + dr, c + dc);
+    if (board[sr][sc] === 'M') {
+        board[sr][sc] = 'X';
+    } else {
+        dfs(sr, sc);
     }
+
+    return board;
 }
