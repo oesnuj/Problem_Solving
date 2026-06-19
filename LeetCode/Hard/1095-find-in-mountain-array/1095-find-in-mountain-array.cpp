2d44aa9c8c69344462a696b2bf2f362cb394c65b
김준서 Junseo Kim <semiwest2@gmail.com>
2026-06-19 12:51:34 +0900

    Time: 4 ms (75.58%), Space: 10 MB (40.33%) - LeetHub

---
+++ LeetCode/Hard/1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -0,0 +1,71 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int n = mountainArr.length();
+        
+        int peak = findPeak(mountainArr, n);
+        
+        int leftResult = searchAscending(mountainArr, 0, peak, target);
+        if (leftResult != -1) return leftResult;
+        return searchDescending(mountainArr, peak + 1, n - 1, target);
+    }
+    
+private:
+    int findPeak(MountainArray &arr, int n) {
+        int st = 0;
+        int en = n - 2;
+
+        while(st < en){
+            int mid = (st + en)/2;
+            if(arr.get(mid) > arr.get(mid+1)) {
+                en = mid;  
+            }
+            else{
+                st = mid + 1;
+            }
+        }
+        return st;
+    }
+    
+    int searchAscending(MountainArray &arr, int left, int right, int target) {
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(arr.get(mid) < target){
+                left = mid + 1;
+            }
+            else if(arr.get(mid) > target){
+                right = mid - 1;
+            }
+            else{
+                return mid; 
+            }
+        }
+        return -1;
+    }
+    
+    int searchDescending(MountainArray &arr, int left, int right, int target) {
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(arr.get(mid) < target) {
+                right = mid - 1;
+            }
+            else if(arr.get(mid) > target) {
+                left = mid + 1;
+            }
+            else{
+                return mid; 
+            }
+        }
+        return -1;
+    }
+};
