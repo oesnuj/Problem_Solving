8d4df3726942135fb88b7e46d8219204b2051df9
김준서 Junseo Kim <semiwest2@gmail.com>
2026-06-25 18:48:27 +0900

    Time: 0 ms (100%), Space: 8.2 MB (42.75%) - LeetHub

--- LeetCode/Medium/1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ LeetCode/Medium/1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
-        double hourAngle = minutes * 0.5 +  hour * 30;
+        double hourAngle = (hour % 12) * 30 + minutes * 0.5;
         double minuteAngle = minutes * 6;
-        double result = abs(hourAngle - minuteAngle);
-        return min(result, 360 - result);
+        double diff = fabs(hourAngle - minuteAngle);
+        return min(diff, 360 - diff);
     }
 };
